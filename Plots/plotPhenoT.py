4613f359ff2ed9a645beb8133d1011e0a6de6c66
Jared Galloway <jaredgalloway07@gmail.com>
2017-09-06 13:15:08 -0700

    DATA

--- Plots/plotPhenoT.py
+++ Plots/plotPhenoT.py
@@ -10,7 +10,7 @@ import sys
 data = []
 x_axis = []
 lake_traces = []
-File = open("../Output1/MyRecipe2_1_1/AveragePheno.txt","r")
+File = open("../Output1/MyRecipe2_2_1/AveragePheno.txt","r")
 first = File.readline()
 numAxis,interval,numLakes = (int(i) for i in first.split())
 
