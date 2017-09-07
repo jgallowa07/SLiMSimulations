fe291b57356fc9d682eb297b76d44bf482794399
jgallowa07 <jaredgalloway07@gmail.com>
2017-09-07 15:42:30 -0700

    New Recipe

--- Plots/plotFst.py
+++ Plots/plotFst.py
@@ -10,7 +10,7 @@ col4 = []
 col5 = []
 
 x_axis = []
-File = open("../Output1/MyRecipe2_2_3/AverageFst.txt","r")
+File = open("../Output1/MyRecipe2_2_4/AverageFst.txt","r")
 first = File.readline()
 numAxis,interval = (int(i) for i in first.split())
 for line in File:
@@ -87,4 +87,4 @@ layout = dict(title = 'Average Fst',
               )
 
 fig = dict(data=data, layout=layout)
-py.iplot(fig, filename='2_2_3 Average Fst')
+py.iplot(fig, filename='2_2_4 Average Fst')
