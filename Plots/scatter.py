55d986ab7649d90e116504f575451522eca51c32
jgallowa07 <jaredgalloway07@gmail.com>
2017-09-08 09:21:15 -0700

    rm 231

--- Plots/scatter.py
+++ Plots/scatter.py
@@ -7,7 +7,7 @@ col1 = []
 col2 = []
 col3 = []
 
-File = open("../Output1/MyRecipe2_2_3/WholeMutations.txt","r")
+File = open("../Output1/MyRecipe2_2_5/WholeMutations.txt","r")
 first = File.readline()
 for line in File:
 	n1, n2, n3= (float(s) for s in line.split())
@@ -38,4 +38,4 @@ layout = dict(title = 'Effect and Frequecy by Position',
 
 fig = dict(data = data, layout = layout)
 	
-py.iplot(fig, filename='2_2_3 Whole Mutations')
+py.iplot(fig, filename='2_2_5 Whole Mutations')
