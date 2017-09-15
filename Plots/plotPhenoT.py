4f41153597b6968365aa24cf18a1944fc8e2dd0f
jgallowa07 <jaredgalloway07@gmail.com>
2017-09-15 10:09:24 -0700

    longer run 2_2_7

--- Plots/plotPhenoT.py
+++ Plots/plotPhenoT.py
@@ -10,7 +10,7 @@ import sys
 data = []
 x_axis = []
 lake_traces = []
-File = open("../Output1/MyRecipe2_2_9/AveragePheno.txt","r")
+File = open("../Output1/MyRecipe2_2_7/AveragePheno.txt","r")
 first = File.readline()
 numAxis,interval,numLakes = (int(i) for i in first.split())
 
@@ -73,4 +73,4 @@ layout = dict(title = 'Average Phenotype',
               )
 
 fig = dict(data=data, layout=layout)
-py.iplot(fig, filename='2_2_9 Average Phenotype')
+py.iplot(fig, filename='2_2_7 Average Phenotype')
