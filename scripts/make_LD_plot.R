9535774c53e0b383bd45c1e2cfe6ae261e7c17dd
peter <petrel.harp@gmail.com>
2017-10-10 11:55:11 -0700

    revised plots

--- scripts/make_LD_plot.R
+++ scripts/make_LD_plot.R
@@ -3,9 +3,10 @@
 # It can be used as a starting point for a more general script
 # that can be applied to other files.
 
-basedir <- "SLiMSimulations/Output1/MyRecipe4_0_5"
+# basedir <- "SLiMSimulations/Output1/MyRecipe4_0_5"
+basedir <- "."
 
-base_strings <- c("oldlake100", "newlake100", "fresh1K", "marine1K")
+base_strings <- rev(c("oldlake100", "newlake100", "fresh1K", "marine1K"))
 
 eff <- scan(file.path(basedir, "effectSizes.txt"))
 
@@ -15,13 +16,24 @@ read_ped <- function (base) {
     ped_data <- data.table::fread(file.path(basedir,paste0(base,".ped")))
     pos <- scan(file.path(basedir,paste0(base,"_pos.txt")))
     # matrix of number of Ts
-    geno <- c(0,1,1,2)[match(as.matrix(ped_data[,-(1:6)]), c("AA", "AT", "TA", "TT"))]
-    dim(geno) <- dim(ped_data) - c(0,6)
+    ## geno <- c(0,1,1,2)[match(as.matrix(ped_data[,-(1:6)]), c("AA", "AT", "TA", "TT"))]
+    dip_geno <- c(0,1)[match(as.matrix(ped_data[,-(1:6)]), c("A", "T"))]
+    dim(dip_geno) <- dim(ped_data) - c(0,6)
+    nindivs <- nrow(dip_geno)
+    # separate out the two haplotypes
+    nsnps <- length(eff)
+    hap1 <- 2*(1:nsnps)-1
+    hap2 <- 2*(1:nsnps)
+    geno <- rbind(dip_geno[,hap1], dip_geno[,hap2])
+    geno <- geno[unlist(rbind(1:nindivs, nindivs+(1:nindivs))),]
     info <- as.data.frame(cbind(ped_data[,1:6], pos))
     names(info) <- c(ped_cols, "geog_loc")
     info <- info[,!grepl("^X", names(info))]
+    info <- info[rep(1:nrow(info),each=2),]
+    info$hap <- rep(c(0,1), nrow(info)/2)
     stopifnot(ncol(geno) == length(eff))
-    return(list(geno=geno, info=info))
+    stopifnot(nrow(geno) == nrow(info))
+    return(list(base=base, geno=geno, info=info))
 }
 
 ld_cols <- colorRampPalette(c("blue", "white", "red"))(64)
@@ -31,13 +43,20 @@ plot_mat <- function (segsites, LD, ...) {
     image(segsites, segsites, LD, zlim=c(-1,1), 
             col=ld_cols, xaxt='n', yaxt='n', ...)
     do_pts <- lower.tri(LD) & !is.na(LD)
-    points(segsites[row(LD)[do_pts]], segsites[col(LD)[do_pts]], 
+    xvals <- segsites[row(LD)[do_pts]]
+    yvals <- segsites[col(LD)[do_pts]] 
+    abline(v=unique(xvals), lty=3, col=adjustcolor("black", 0.1))
+    abline(h=unique(xvals), lty=3, col=adjustcolor("black", 0.1))
+    points(jitter(xvals, amount=max(segsites)/40), 
+            jitter(yvals, amount=max(segsites)/40), 
             pch=20, col=ld_col_fn(LD[do_pts]))
 }
 
-plot_ld <- function (base1, base2) {
-    pop1 <- read_ped(base1)
-    pop2 <- read_ped(base2)
+plot_ld <- function (pop1, pop2) {
+    base1 <- pop1$base
+    base2 <- pop2$base
+    # pop1 <- read_ped(base1)
+    # pop2 <- read_ped(base2)
 
     # segregating sites
     nindivs <- c(nrow(pop1$geno), nrow(pop2$geno))
@@ -83,12 +102,14 @@ plot_ld <- function (base1, base2) {
             main='cor', xaxt='n', xlab='', ylab='', pch=22, col=ld_cols, bg=ld_cols, cex=3)
     dev.off()
 
-    invisible(return(list(base1=base1, base2=base2, LD=LD)))
+    return(invisible(list(base1=base1, base2=base2, LD=LD)))
 }
 
 for (j in seq_along(base_strings)[-length(base_strings)]) {
+    pop1 <- read_ped(base_strings[j])
     for (k in seq(j+1,length(base_strings))) {
-        plot_ld(base_strings[j], base_strings[k])
+        pop2 <- read_ped(base_strings[k])
+        plot_ld(pop1, pop2)
     }
 }
 
