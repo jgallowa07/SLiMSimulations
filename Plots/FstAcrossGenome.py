a9732c02782248edb0591a0d9fcc7678d5bb8942
jgallowa07 <jaredgalloway07@gmail.com>
2018-01-09 10:20:57 -0800

    Fstplotscrips

---
+++ Plots/FstAcrossGenome.py
@@ -0,0 +1,48 @@
+import plotly
+from plotly.graph_objs import Scatter, Layout
+import plotly.graph_objs as go
+import plotly.plotly as py
+import sys
+
+RecipeNum = "8_0_4"
+data = []
+
+def plotThis(Fi,pop1,pop2,rgb):
+
+	File = open(Fi,"r")
+	first = File.readline()
+
+	Fst = list(map(float,File.readline().split()))
+	Pos = list(map(int,File.readline().split()))
+
+	Fst_Sorted = [fst for _,fst in sorted(zip(Pos,Fst))]
+	Pos_Sorted = sorted(Pos)
+
+	trace = go.Scatter(
+	    x = Pos_Sorted,
+	    y = Fst_Sorted,
+	    name = 'Fst('+pop1+','+pop2+')',
+	    line = dict(
+		color = (rgb),
+		width = 1
+			)
+	)
+
+	data.append(trace)
+
+	# Edit the layout
+
+plotThis("../Output1/MyRecipe"+RecipeNum+"/FreshwaterFreshwater2Fst.txt","Freshwater","IntroducedFreshwater",'rgb(60,179,113)')
+plotThis("../Output1/MyRecipe"+RecipeNum+"/OceanFreshwaterFst.txt","Ocean","Freshwater",'rgb(127,255,212)')
+plotThis("../Output1/MyRecipe"+RecipeNum+"/OceanFreshwater2Fst.txt","Ocean","IntroducedFreshwater",'rgb(138,43,226)')
+
+layout = dict(
+	title = 'Fst per Mutation',
+	xaxis = dict(title = 'Chromosome Position'),
+	yaxis = dict(title = 'Fst'),
+	      )
+
+fig = dict(data=data, layout=layout)
+plotly.offline.plot(fig, filename=RecipeNum + 'FstChromosome.html')
+
+
--- Plots/plotFst.py
+++ Plots/plotFst.py
@@ -3,96 +3,169 @@ from plotly.graph_objs import Scatter, Layout
 import plotly.graph_objs as go
 import plotly.plotly as py
 import sys
-col1 = []
-col2 = []
-col3 = []
-col4 = []
-col5 = []
-col6 = []
-col7 = []
 
-x_axis = []
-File = open("../Output1/MyRecipe3_0_1/AverageFst.txt","r")
+dataFromFile = []
+dataFromFile2 = []
+traceNames = []
+traceColors = []
+traceNames2 = []
+traceColors2 = []
+xAxis = []
+xAxis2 = []
+Traces = []
+
+File = open("../Output1/MyRecipe8_0_5/AverageFst.txt","r")
+File2 = open("../Output1/MyRecipe8_0_5/AverageFst2.txt","r")
 first = File.readline()
+first2 = File2.readline()
 numAxis,interval = (int(i) for i in first.split())
-for line in File:
-	n1,n2,n3,n4,n5,n6,n7 = (float(s) for s in line.split())
-	#n1 = float(line)
-	col1.append(n1)
-	col2.append(n2)
-	col3.append(n3)
-	col4.append(n4)
-	col5.append(n5)
-	col6.append(n6)
-	col7.append(n7)
+numAxis2,interval2,introduce = (int(i) for i in first2.split())
 
-print("numAxis: "+str(numAxis))	
+for i in range(0,9):
+	col = []
+	dataFromFile.append(col)
+
+for i in range(0,9):
+	col = []
+	dataFromFile2.append(col)
+
+for line in File:
+	lineData = line.split()
+	for i in range(0,9):
+		dataFromFile[i].append(lineData[i])
+
+for line in File2:
+	lineData = line.split()
+	for i in range(0,9):
+		dataFromFile2[i].append(lineData[i])
 
 for i in range(0,numAxis + 1):
-	x_axis.append(i*interval)
-print(x_axis)	
+	xAxis.append(i*interval)
 
-trace0 = go.Scatter(
-    x = x_axis,
-    y = col1,
-    name = 'Average Fst Marine/Freshwater',
-    line = dict(
-        color = ('rgb(25, 12, 24)'),
-        width = 1)
-)
-trace1 = go.Scatter(
-    x = x_axis,
-    y = col2,
-    name = 'Average Fst between Lakes',
-    line = dict(
-        color = ('rgb(225, 96, 167)'),
-        width = 1,)
-)
-trace2 = go.Scatter(
-    x = x_axis,
-    y = col3,
-    name = 'Average Fst between Oceanic pop',
-    line = dict(
-	color = ('rgb(5, 125, 24)'),
-	width = 1) # dash options include 'dash', 'dot', and 'dashdot'
-)
-trace3 = go.Scatter(
-    x = x_axis,
-    y = col4,
-    name = 'Average Fst Marine/Freshwater (Neutral Mutations only)',
-    line = dict(
-	color = ('rgb(22, 96, 167)'),
-	width = 1,
-	dash = 'dot')
-)
-trace4 = go.Scatter(
-    x = x_axis,
-    y = col5,
-    name = 'Average Fst Marine/Freshwater (Effect Mutations only)',
-    line = dict(
-        color = ('rgb(205, 12, 24)'),
-        width = 1,
-        dash = 'dot')
-)
-trace5 = go.Scatter(
-    x = x_axis,
-    y = col6,
-    name = 'Average Fst Between Lakes (Neutral Mutations only)',
-    line = dict(
-        color = ('rgb(5, 112, 4)'),
-        width = 1,
-        dash = 'dot')
-)
-trace6 = go.Scatter(
-    x = x_axis,
-    y = col7,
-    name = 'Average Fst Between Lakes (Effect Mutations only)',
-    line = dict(
-        color = ('rgb(25, 172, 124)'),
-        width = 1,
-        dash = 'dot')
-)
-data = [trace0,trace1,trace2,trace3,trace4,trace5,trace6]
+for i in range(0,numAxis2 + 1):
+	xAxis2.append(i*interval2+introduce)	
+
+
+trace1name = 'Marine,Lakes'
+trace1color = 'rgb(178,34,34)'
+traceNames.append(trace1name)
+traceColors.append(trace1color)
+
+trace2name = 'Marine,Lakes:Neutral'
+trace2color = 'rgb(233,150,122)'
+traceNames.append(trace2name)
+traceColors.append(trace2color)
+
+trace3name = 'Marine,Lakes:Effect'
+trace3color = 'rgb(255,140,0)'
+traceNames.append(trace3name)
+traceColors.append(trace3color)
+
+trace4name = 'Lakes'
+trace4color = 'rgb(189,183,107)'
+traceNames.append(trace4name)
+traceColors.append(trace4color)
+
+trace5name = 'Lakes:Neutral'
+trace5color = 'rgb(128,128,0)'
+traceNames.append(trace5name)
+traceColors.append(trace5color)
+
+trace6name = 'Lakes:Effect'
+trace6color = 'rgb(34,139,34)'
+traceNames.append(trace6name)
+traceColors.append(trace6color)
+
+trace7name = 'Marine'
+trace7color = 'rgb(47,27,27)'
+traceNames.append(trace7name)
+traceColors.append(trace7color)
+
+trace7name = 'Marine:Neutral'
+trace7color = 'rgb(147,27,27)'
+traceNames.append(trace7name)
+traceColors.append(trace7color)
+
+trace7name = 'Marine:Effect'
+trace7color = 'rgb(47,127,27)'
+traceNames.append(trace7name)
+traceColors.append(trace7color)
+
+#--------------------------
+
+trace8name = 'Marine,Introduced'
+trace8color = 'rgb(0,128,128)'
+traceNames2.append(trace8name)
+traceColors2.append(trace8color)
+
+trace9name = 'Marine,Intro:Neutral'
+trace9color = 'rgb(64,224,208)'
+traceNames2.append(trace9name)
+traceColors2.append(trace9color)
+
+trace10name = 'Marine,Intro:Effect'
+trace10color = 'rgb(127,255,212)'
+traceNames2.append(trace10name)
+traceColors2.append(trace10color)
+
+trace11name = 'Introduced'
+trace11color = 'rgb(139,0,139)'
+traceNames2.append(trace11name)
+traceColors2.append(trace11color)
+
+trace12name = 'Introduced:Neutral'
+trace12color = 'rgb(216,160,221)'
+traceNames2.append(trace12name)
+traceColors2.append(trace12color)
+
+trace13name = 'Introduced:Effect'
+trace13color = 'rgb(199,21,133)'
+traceNames2.append(trace13name)
+traceColors2.append(trace13color)
+
+trace14name = 'Lakes,Introduced'
+trace14color = 'rgb(160,82,45)'
+traceNames2.append(trace14name)
+traceColors2.append(trace14color)
+
+trace15name = 'Lakes,Intro:Neutral'
+trace15color = 'rgb(210,105,30)'
+traceNames2.append(trace15name)
+traceColors2.append(trace15color)
+
+trace16name = 'Lakes,Intro:Effect'
+trace16color = 'rgb(188,143,143)'
+traceNames2.append(trace16name)
+traceColors2.append(trace16color)
+
+
+for i in range(0,9):
+
+	trace = go.Scatter(
+	    x = xAxis,
+	    y = dataFromFile[i],
+	    name = traceNames[i],
+	    line = dict(
+		color = (traceColors[i]),
+		width = 1)
+	)
+
+	Traces.append(trace)
+	
+for i in range(0,9):
+
+	trace = go.Scatter(
+	    x = xAxis2,
+	    y = dataFromFile2[i],
+	    name = traceNames2[i],
+	    line = dict(
+		color = (traceColors2[i]),
+		width = 1)
+	)
+
+	Traces.append(trace)
+
+data = Traces
 
 # Edit the layout
 layout = dict(title = 'Average Fst',
@@ -101,4 +174,5 @@ layout = dict(title = 'Average Fst',
               )
 
 fig = dict(data=data, layout=layout)
-py.iplot(fig, filename='3_0_1 Average Fst')
+#py.iplot(fig, filename='3_0_1 Average Fst')
+plotly.offline.plot(fig, filename='8_0_5Fst.html')
