55b03d803cbdd9bdd42f5b235d86f68f60f39798
jgallowa07 <jaredgalloway07@gmail.com>
2018-06-27 16:50:13 -0700

    FstRoc

--- Output1/stitcher.py
+++
@@ -1,7 +0,0 @@
-t = open("myOut176_Oc.txt","r");
-z = open("myOut176_Fr.txt","r");
-tempfiles = [t,z]
-
-f = open("myOut176_Wh.txt", "w")
-for tempfile in tempfiles:
-    f.write(tempfile.read())
--- Plots/TESTS/TestHist.py
+++
@@ -1,9 +0,0 @@
-import plotly.plotly as py
-import plotly.graph_objs as go
-
-import numpy as np
-
-x = np.random.randn(500)
-data = [go.Histogram(x=x)]
-
-py.iplot(data, filename='basic histogram')
